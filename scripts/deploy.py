80ab5dd7a59587034ebd4b50fd7f660b6b3a583b
Akashchem <akashverma.che18@itbhu.ac.in>
2022-03-02 16:34:21 +0530

    first one

---
+++ scripts/deploy.py
@@ -0,0 +1,51 @@
+from pickle import TRUE
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpscipts import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+
+
+def main():
+    deploy_fund_me()
+    fund()
+    withdraw()
+
+
+print(network.show_active)
+
+
+def deploy_fund_me():
+    account = get_account()
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    # return fund_me
+
+
+# print(f"Contract deployed to {fund_me.address}")
+# return fund_me
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
